171583a92a3a6ed9567f95ddcae59766ce25d77b
Kirill Nevzorov <kirilos24@gmail.com>
2018-01-14 03:07:59 +0000

    changed server to serve browserified js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const connect = require('connect');
 const serve = require('serve-static');
 const compression = require('compression');
+const browserify = require('browserify-middleware');
 const path = require('path');
 
 const app = connect();
-	
+
 app.use(compression());
+app.use('/index.js',browserify('index.js'));
 app.use(serve(path.join(__dirname,'www')));
 
 app.listen(8080);
-
