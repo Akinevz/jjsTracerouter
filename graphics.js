d7cf2d455bd0c6b3828bab0c71a4fa703d863808
Kirill Nevzorov <kirilos24@gmail.com>
2018-01-14 05:46:01 +0000

    made skybox work
    added media
    organised graphics code

--- graphics.js
+++ graphics.js
@@ -1,74 +1,98 @@
 const THREE = require('three');
 const OrbitControls = require('three-orbit-controls')(THREE);
+
+
+const textures = {
+    earth: new THREE.TextureLoader().load('texture.png'),
+    stars: new THREE.TextureLoader().load('starmap.png'),
+};
+
+const materials = {
+    normal: new THREE.MeshNormalMaterial({
+        flatShading:true,
+        side:THREE.DoubleSide
+    }),
+    earth: new THREE.MeshBasicMaterial({
+        map: textures.earth
+    }),
+    stars: new THREE.MeshBasicMaterial({
+        map: textures.stars,
+        side:THREE.BackSide
+        
+    }),
+}
+
+
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 4000);
+
+const sizes = {
+    earth: 500,
+    stars: 2000
+};
+
+const stars = new THREE.Mesh(new THREE.SphereBufferGeometry(
+    sizes.stars,
+    100,
+    100
+), materials.stars);
+
+const earth = new THREE.Mesh(new THREE.SphereBufferGeometry(
+    sizes.earth,
+    sizes.earth,
+    sizes.earth
+), materials.earth);
+
+const meshes = [
+    stars, earth
+];
+
+const onWindowResize = function () {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+
+
 module.exports = function (targ) {
     console.log("will initialise p5 on", canvas);
 
+    /* set up scene */
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-    const texture = new THREE.TextureLoader().load('texture.png');
-    const geometry = new THREE.SphereBufferGeometry(200, 200, 200);
-    const uk_geo = new THREE.PlaneGeometry(500,500);
-    const colourful = new THREE.MeshNormalMaterial({
-        side:THREE.DoubleSide
-    });
-    const material = new THREE.MeshBasicMaterial({
-        map: texture
-    });
     const controls = new OrbitControls(camera);
-    controls.enablePan = false;
-    console.log(camera.far);
-    controls.maxDistance = camera.far;
-    geometry.computeBoundingSphere();
-    controls.minDistance = geometry.boundingSphere.radius * 1.2;
-    console.log(controls.minDistance)
-    console.log(controls)
-    const mesh = new THREE.Mesh(geometry, material);
-    const uk_mesh = new THREE.Mesh(uk_geo,colourful);
     const renderer = new THREE.WebGLRenderer({
         canvas: targ
     });
 
-    init();
+    const animate = function () {
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    }
+
+    window.addEventListener('resize', onWindowResize, false);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    earth.geometry.computeBoundingSphere();
+
+    controls.enablePan = false;
+    controls.maxDistance = sizes.stars*0.90;
+    controls.minDistance = earth.geometry.boundingSphere.radius * 1.2;
+    controls.target = new THREE.Vector3();
+
+    camera.position.z = (controls.maxDistance - controls.minDistance) / 2;
+    camera.lookAt(controls.center);
+
+    stars.position = camera.position;
+    
+    for (mesh of meshes) {
+        scene.add(mesh);
+    }
+
     animate();
 
-    function init() {
-
-        camera.position.z = (camera.far - camera.near)/2;
-        uk_mesh.position.x = 300;
-        controls.target = new THREE.Vector3();
-        camera.lookAt(controls.center);
-        scene.add(mesh);
-        
-        scene.add(uk_mesh);
-
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        //        document.body.appendChild(renderer.domElement);
-
-        //
-
-        window.addEventListener('resize', onWindowResize, false);
-
-    }
-
-    function onWindowResize() {
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize(window.innerWidth, window.innerHeight);
-
-    }
-
-    function animate() {
-
-        requestAnimationFrame(animate);
-
-        //        camera.position.x += 0.005;
-        //        camera.position.y += 0.01;
-
-        renderer.render(scene, camera);
-
-    }
     return renderer.domElement;
 }
